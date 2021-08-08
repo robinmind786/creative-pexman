17bf7d0e0264db99f8745097521ca12122efe47e
Robin Mind <43693130+robinmind786@users.noreply.github.com>
2021-08-08 21:08:19 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-const l = 0;
+// Slider 
+var currentItem = 3;
+showSlides(currentItem);
+showSlides();
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName('mySlides');
+	if(n > slides.length) {
+		currentItem = 1;
+	}
+	if(n < 1) {
+		currentItem = slides.length;
+	}
+	currentItem++;
+	if(currentItem > slides.length) {
+		currentItem = 1
+	}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+	slides[currentItem-1].style.display = 'block';
+	setTimeout(showSlides, 5000);
+}
